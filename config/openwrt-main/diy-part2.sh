fe72ea17baaed23cc8f15906231550c7077d0a4b
niunaixiaoshi <159025478+niunaixiaoshi@users.noreply.github.com>
2024-02-06 04:39:37 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -16,14 +16,21 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.8.1/192.168.31.4/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+ svn co https://github.com/kenzok8/small-package/tree/main/luci-app-openclash package/luci-app-openclash
+ svn co https://github.com/kenzok8/small-package/tree/main/adguardhome package/adguardhome
+ svn co https://github.com/kenzok8/small-package/tree/main/mosdns package/mosdns
+ svn co https://github.com/kenzok8/small-package/tree/main/luci-app-store package/luci-app-store
+ svn co https://github.com/HiGarfield/lede-17.01.4-test/tree/master/package/extra/luci-app-usb-printer package/luci-app-usb-printer
+
+
 
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
